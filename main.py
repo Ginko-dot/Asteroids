ad4b54a50864a5cd959538ff8ccdd02662abf0ff
Ginko-dot <jmseel@gmx.de>
2025-11-03 17:28:04 +0100

    implemented fps limiter

--- main.py
+++ main.py
@@ -3,16 +3,22 @@ from constants import *
 
 def main():
     pygame.init
+    frame_limiter = pygame.time.Clock()
+    dt= 0
+    screen=pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+
     print("Starting Asteroids!")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
-    screen=pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
         pygame.display.flip
+        dt=frame_limiter.tick(60)/1000
+        print (dt)
 
 if __name__ == "__main__":
     main()
