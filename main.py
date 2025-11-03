a9fb4c1aa91fdf2c08a9f7ddd8f9ade8ddb42c63
Ginko-dot <jmseel@gmx.de>
2025-11-03 18:12:21 +0100

    added playersprite, nothing moves yet

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init
     frame_limiter = pygame.time.Clock()
     dt= 0
     screen=pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     print("Starting Asteroids!")
     print("Screen width:", SCREEN_WIDTH)
@@ -16,7 +18,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
-        pygame.display.flip
+        player.draw(screen)
+        pygame.display.flip()
         dt=frame_limiter.tick(60)/1000
         print (dt)
 
