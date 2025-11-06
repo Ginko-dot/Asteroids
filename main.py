5448cc73d7bced19ad4b8481b939e8ce386824c4
Ginko-dot <jmseel@gmx.de>
2025-11-06 22:23:12 +0100

    we´re thinking in groups now

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
 import pygame
+from logger import log_state
 from constants import *
 from player import *
 
 def main():
-    pygame.init
+    pygame.init()
     frame_limiter = pygame.time.Clock()
     dt= 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable,drawable)
     screen=pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    #print("updatable size:", len(updateable))
+    #print("drawable size:", len(drawable))
+
 
     print("Starting Asteroids!")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
     
     while True:
+        log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+        updatable.update(dt)
         screen.fill((0,0,0))
-        player.draw(screen)
+        for entity in drawable:
+            entity.draw(screen)
+            #print("drawing:", entity)
         pygame.display.flip()
         dt=frame_limiter.tick(60)/1000
-        print (dt)
+        #print (dt)
 
 if __name__ == "__main__":
     main()
